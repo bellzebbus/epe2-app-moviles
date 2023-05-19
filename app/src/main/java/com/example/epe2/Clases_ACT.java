b002b893a26cf75bd495755a14eba86146bf78a6
Sebastian Arias <zebbas@outlook.com>
2023-05-19 16:09:00 -0400

    creacion Clases_ACT

---
+++ app/src/main/java/com/example/epe2/Clases_ACT.java
@@ -0,0 +1,14 @@
+package com.example.epe2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Clases_ACT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clases);
+    }
+}
